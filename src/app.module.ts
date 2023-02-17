6c1bb21b28464c07e2e8ec31b34440cb5d5f8716
monicashdias <msumiedias@gmail.com>
2023-02-17 10:28:56 -0300

    feat: generate resources for user, game and product

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { GameModule } from './game/game.module';
+import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [ProfileModule, PrismaModule],
+  imports: [ProfileModule, PrismaModule, GameModule, UserModule, GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
