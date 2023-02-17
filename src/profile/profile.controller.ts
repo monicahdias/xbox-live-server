6e690bcb5dd5d7cef788e239573726d50b1936a5
monicashdias <msumiedias@gmail.com>
2023-02-16 21:13:29 -0300

    feat(profile): add 'create' endpoint

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,10 +1,18 @@
 import { Controller } from '@nestjs/common/decorators/core/controller.decorator';
-import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
+import {
+  Get,
+  Post,
+} from '@nestjs/common/decorators/http/request-mapping.decorator';
 
 @Controller('profile')
 export class ProfileController {
   @Get()
   findAll() {
     return 'Buscar todos os perfis';
+  }
+
+  @Post()
+  create() {
+    return 'Criar um perfil';
   }
 }
