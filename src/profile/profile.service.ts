272da9a66f53b6be0655f800af1a8aca79316c6c
monicashdias <msumiedias@gmail.com>
2023-02-16 21:43:59 -0300

    feat(profile): implement create table endpoint

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
 import { CreateProfileDto } from './dto/create-profile.dto';
+import { Profile } from './entities/profile.entity';
 
 @Injectable()
 export class ProfileService {
+  profiles: Profile[] = [];
+
   findAll() {
     return 'Buscar todos os perfis';
   }
   create(createProfileDto: CreateProfileDto) {
-    return 'Criar um perfil' + JSON.stringify(createProfileDto);
+    const profile: Profile = { id: 'random_id', ...createProfileDto };
+
+    this.profiles.push(profile);
+
+    return profile;
   }
 }
