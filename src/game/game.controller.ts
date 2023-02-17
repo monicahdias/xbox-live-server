943effeef0ef9b7b7b9635d3fdbd6aa422eaf69f
monicashdias <msumiedias@gmail.com>
2023-02-17 10:37:59 -0300

    feat(game): implement controller

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,34 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { GameService } from './game.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { HttpCode, HttpStatus } from '@nestjs/common';
 
+@ApiTags('game')
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
 
-  @Post()
-  create(@Body() createGameDto: CreateGameDto) {
-    return this.gameService.create(createGameDto);
-  }
-
   @Get()
+  @ApiOperation({ summary: 'Listar todos os jogos' })
   findAll() {
     return this.gameService.findAll();
   }
 
+  @Post()
+  @ApiOperation({ summary: 'Criar um jogo' })
+  create(@Body() createGameDto: CreateGameDto) {
+    return this.gameService.create(createGameDto);
+  }
+
   @Get(':id')
+  @ApiOperation({ summary: 'Visualizar um jogo' })
   findOne(@Param('id') id: string) {
-    return this.gameService.findOne(+id);
+    return this.gameService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Editar um jogo' })
   update(@Param('id') id: string, @Body() updateGameDto: UpdateGameDto) {
-    return this.gameService.update(+id, updateGameDto);
+    return this.gameService.update(id, updateGameDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gameService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Remover um jogo' })
+  delete(@Param('id') id: string) {
+    this.gameService.delete(id);
   }
 }
