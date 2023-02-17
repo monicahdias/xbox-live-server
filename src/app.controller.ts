0bcbc70db249f075c88dba2eefce67fb66510b9f
monicashdias <msumiedias@gmail.com>
2023-02-16 22:19:41 -0300

    feat: install swagger and validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger/dist';
 import { AppService } from './app.service';
 
+@ApiTags('status')
 @Controller('/app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getAppStatus();
   }
 }
