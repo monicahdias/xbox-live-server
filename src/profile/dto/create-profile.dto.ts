d534b54a402e7d8d09d17e1628e714b556e36489
monicashdias <msumiedias@gmail.com>
2023-02-16 22:22:47 -0300

    feat(profile): add validator and ApiProperty in create-profile.dto.ts

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,4 +1,19 @@
+import { IsString } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger/dist/decorators';
+
 export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do perfil',
+    example: 'KillerPenguin',
+  })
   title: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'URL da imagem do perfil',
+    example:
+      'https://openseauserdata.com/files/18d1719179758dbf743a7cc9a3540b72.jpg',
+  })
   imageUrl: string;
 }
