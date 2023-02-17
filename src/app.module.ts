e5ea1e058f72dfc8e39b9e0d63fb0dfd293c7443
monicashdias <msumiedias@gmail.com>
2023-02-16 23:22:04 -0300

    feat(prisma): create module & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProfileModule],
+  imports: [ProfileModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
