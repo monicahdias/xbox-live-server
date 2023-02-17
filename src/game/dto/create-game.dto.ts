bd408a4e55285b80b93ae99e6b08dca5fd7a4bd5
monicashdias <msumiedias@gmail.com>
2023-02-17 11:01:50 -0300

    feat(game): implement dto validation

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,12 +1,57 @@
+import { IsNumber, IsUrl } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateGameDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do jogo',
+    example: 'Valorant',
+  })
   title: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'URL da imagem do jogo',
+    example: 'https://sm.ign.com/ign_br/game/v/valorant/valorant_5mxf.jpg',
+  })
   coverImageUrl: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do jogo',
+    example:
+      'Valorant é um jogo eletrônico multijogador gratuito para jogar de tiro em primeira pessoa desenvolvido e publicado pela Riot Games.',
+  })
   description: string;
+
+  @IsNumber()
+  @ApiProperty({
+    description: 'Ano de lançamento do jogo',
+    example: 2020,
+  })
   year: number;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Nota do IMDb do jogo',
+    example: 4.98,
+  })
   imdbScore: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'URL do trailer do jogo no YouTube',
+    example: 'https://youtu.be/IoUFmdDxjvk',
+  })
   trailerYouTubeUrl: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'URL da gameplay do jogo no YouTube',
+    example: 'https://www.youtube.com/watch?v=gRqddeZDNyM',
+  })
   gameplayYouTubeUrl: string;
 }
