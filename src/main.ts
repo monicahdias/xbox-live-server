a3af167ec914d006075249ae7cb6b9ab099c7908
monicashdias <msumiedias@gmail.com>
2023-02-16 22:21:56 -0300

    feat(main): add validation and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  //Swagger
+  const config = new DocumentBuilder()
+    .setTitle('XboxLive')
+    .setDescription('Game server do Xbox Live')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('user')
+    .addTag('profile')
+    .addTag('game')
+    .addTag('genre')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
